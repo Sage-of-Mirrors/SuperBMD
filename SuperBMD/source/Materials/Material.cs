c47f89958ae7101dcfa1683306bb5fa738e531cc
Sage_of_Mirrors <empressglatia@gmail.com>
2017-10-12 18:51:24 -0500

    Added Color (components are floats) and Color32 (components are bytes) and J3dUtility, which converts between the J3D and GX versions of the AttenuationFn and ColorChannelID enums.

--- SuperBMD/source/Materials/Material.cs
+++ SuperBMD/source/Materials/Material.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperBMD.Materials.Enums;
+using SuperBMD.Util;
 
 namespace SuperBMD.Materials
 {
     class Material
     {
+        public string Name;
+        public byte Flag;
+        public byte ColorChannelControlsCount;
+        public byte NumTexGensCount;
+        public byte NumTevStagesCount;
+        public IndirectTexturing IndTexEntry;
+        public CullMode CullMode;
+        public Color?[] MaterialColors;
+        public ChannelControl[] ChannelControls;
+        public Color?[] AmbientColors;
+        public Color?[] LightingColors;
+        public TexCoordGen[] TexCoord1Gens;
+        public TexCoordGen[] TexCoord2Gens;
+        public TexMatrix[] TexMatrix1;
+        public TexMatrix[] TexMatrix2;
+        public BinaryTextureImage[] Textures;
+        public TevOrder[] TevOrders;
+        public KonstColorSel[] ColorSels;
+        public KonstAlphaSel[] AlphaSels;
+        public Color?[] TevColors;
+        public Color?[] KonstColors;
+        public TevStage[] TevStages;
+        public TevSwapMode[] SwapModes;
+        public TevSwapModeTable[] SwapTables;
+        public Fog FogInfo;
+        public AlphaCompare AlphCompare;
+        public BlendMode BMode;
+        public ZMode ZMode;
+        public bool ZCompLoc;
+        public bool Dither;
     }
 }
---
+++ SuperBMD/source/Util/J3DUtility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperBMD.Materials.Enums;
+
+namespace SuperBMD.Util
+{
+    public static class J3DUtility
+    {
+        public static GXColorChannelId ToGXColorChannelId(this J3DColorChannelId id)
+        {
+            switch (id)
+            {
+                case J3DColorChannelId.Color0: return GXColorChannelId.Color0;
+                case J3DColorChannelId.Color1: return GXColorChannelId.Color1;
+                case J3DColorChannelId.Alpha0: return GXColorChannelId.Alpha0;
+                case J3DColorChannelId.Alpha1: return GXColorChannelId.Alpha1;
+            }
+
+            throw new ArgumentOutOfRangeException("id");
+        }
+
+        public static J3DColorChannelId ToJ3DColorChannelId(this GXColorChannelId id)
+        {
+            switch (id)
+            {
+                case GXColorChannelId.Color0: return J3DColorChannelId.Color0;
+                case GXColorChannelId.Color1: return J3DColorChannelId.Color1;
+                case GXColorChannelId.Alpha0: return J3DColorChannelId.Alpha0;
+                case GXColorChannelId.Alpha1: return J3DColorChannelId.Alpha1;
+            }
+
+            throw new ArgumentOutOfRangeException("id");
+        }
+
+        public static GXAttenuationFn ToGXAttenuationFn(this J3DAttenuationFn fn)
+        {
+            switch (fn)
+            {
+                case J3DAttenuationFn.None_0:
+                case J3DAttenuationFn.None_2: return GXAttenuationFn.None;
+                case J3DAttenuationFn.Spec:   return GXAttenuationFn.Spec;
+                case J3DAttenuationFn.Spot:   return GXAttenuationFn.Spot;
+            }
+
+            throw new ArgumentOutOfRangeException("fn");
+        }
+
+        public static J3DAttenuationFn ToJ3DAttenuationFn(this GXAttenuationFn fn)
+        {
+            switch (fn)
+            {
+                case GXAttenuationFn.None: return J3DAttenuationFn.None_0;
+                case GXAttenuationFn.Spec: return J3DAttenuationFn.Spec;
+                case GXAttenuationFn.Spot: return J3DAttenuationFn.Spot;
+            }
+
+            throw new ArgumentOutOfRangeException("fn");
+        }
+    }
+}
