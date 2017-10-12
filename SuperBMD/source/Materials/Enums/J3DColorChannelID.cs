a93c996cdf31cf9cf0061805827967960d762028
Sage_of_Mirrors <empressglatia@gmail.com>
2017-10-12 17:53:38 -0500

    Added enums for GX and J3D material data.

---
+++ SuperBMD/source/Materials/Enums/J3DColorChannelID.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBMD.Materials.Enums
+{
+    public enum J3DColorChannelId
+    {
+        Color0 = 0,
+        Alpha0 = 1,
+        Color1 = 2,
+        Alpha1 = 3,
+    }
+}
