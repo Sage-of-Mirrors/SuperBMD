fd9abed7c1e3347cb0b3f3b10c83d3bd62bc2616
Sage_of_Mirrors <empressglatia@gmail.com>
2017-10-13 23:32:11 -0500

    Added enums associated with geometry.

---
+++ SuperBMD/source/Geometry/Enums/GXDataType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBMD.Geometry.Enums
+{
+    public enum GXDataType
+    {
+        Unsigned8  = 0x0,
+        Signed8    = 0x1,
+        Unsigned16 = 0x2,
+        Signed16   = 0x3,
+        Float32    = 0x4
+    }
+}
---
+++ SuperBMD/source/Geometry/Enums/VertexInputType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBMD.Geometry.Enums
+{
+    public enum VertexInputType
+    {
+        None,
+        Direct,
+        Index8,
+        Index16
+    }
+}
