767a0d2ab9a81b1032578649be23cb0d5c6bedcb
Sage_of_Mirrors <empressglatia@gmail.com>
2018-07-08 23:17:45 -0500

    Moved new parameter parsing code to an Arguments struct. Also renamed the containing folders to match the new project names.

---
+++ SuperBMD/Arguments.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SuperBMD
+{
+    /// <summary>
+    /// Container for arguments taken from the user's input.
+    /// </summary>
+    public struct Arguments
+    {
+        public string input_path;
+        public string output_path;
+        public string materials_path;
+        public string texheaders_path;
+        public string tristrip_mode;
+        public bool rotate_model;
+        public bool fix_normals;
+
+        /// <summary>
+        /// Initializes a new Arguments instance from the arguments passed in to SuperBMD.
+        /// </summary>
+        /// <param name="args">Arguments from the user</param>
+        public Arguments(string[] args)
+        {
+            input_path = "";
+            output_path = "";
+            materials_path = "";
+            texheaders_path = "";
+            tristrip_mode = "static";
+            rotate_model = false;
+            fix_normals = true;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i + 1 >= args.Length)
+                    throw new Exception("The parameters were malformed.");
+
+                switch (args[i])
+                {
+                    case "-i":
+                    case "--input":
+                        input_path = args[i + 1];
+                        break;
+                    case "-o":
+                    case "--output":
+                        output_path = args[i + 1];
+                        break;
+                    case "-m":
+                    case "--materialPresets":
+                        materials_path = args[i + 1];
+                        break;
+                    case "-x":
+                    case "--texHeaders":
+                        texheaders_path = args[i + 1];
+                        break;
+                    case "-t":
+                    case "--tristrip":
+                        tristrip_mode = args[i + 1].ToLower();
+                        break;
+                    case "-r":
+                    case "--rotate":
+                        rotate_model = true;
+                        break;
+                    case "-f":
+                    case "--fixNormals":
+                        fix_normals = false;
+                        break;
+                    default:
+                        throw new Exception($"Unknown parameter \"{ args[i] }\"");
+                }
+
+                // Increment the counter to skip to the next parameter
+                i++;
+            }
+
+            ValidateArgs();
+        }
+
+        /// <summary>
+        /// Ensures that all the settings parsed from the user's input are valid.
+        /// </summary>
+        /// <param name="args">Array of settings parsed from the user's input</param>
+        private void ValidateArgs()
+        {
+            // Input
+            if (input_path == "")
+                throw new Exception("No input file was specified.");
+            if (!File.Exists(input_path))
+                throw new Exception($"Input file \"{ input_path }\" does not exist.");
+
+            // Output
+            if (output_path == "")
+            {
+                string input_without_ext = Path.GetFileNameWithoutExtension(input_path);
+
+                if (input_path.EndsWith(".bmd") || input_path.EndsWith(".bdl"))
+                    output_path = input_without_ext + ".dae";
+                else
+                    output_path = input_without_ext + ".bmd";
+            }
+
+            // Material presets
+            if (materials_path != "")
+            {
+                if (!File.Exists(materials_path))
+                    throw new Exception($"Material presets file \"{ materials_path }\" does not exist.");
+            }
+
+            // Texture headers
+            if (texheaders_path != "")
+            {
+                if (!File.Exists(texheaders_path))
+                    throw new Exception($"Texture headers file \"{ texheaders_path }\" does not exist.");
+            }
+
+            // Tristrip options
+            if (tristrip_mode != "static" && tristrip_mode != "all" && tristrip_mode != "none")
+                throw new Exception($"Unknown tristrip option \"{ tristrip_mode }\".");
+        }
+    }
+}
---
+++ SuperBMD/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using SuperBMD;
+
+namespace SuperBMD_UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "-h" || args[0] == "--help")
+            {
+                DisplayHelp();
+                return;
+            }
+
+            Arguments cmd_args = new Arguments(args);
+
+            /*
+            if (args.Length > 0)
+            {
+                if (args[0] == "help")
+                {
+                    DisplayHelp();
+                    return;
+                }
+
+                Model mod = Model.Load(args[0]);
+
+                if (args[0].EndsWith(".bmd") || args[0].EndsWith(".bdl"))
+                    mod.ExportAssImp(args[0], "dae", new ExportSettings());
+                else
+                    mod.ExportBMD(args[0] + ".bmd");
+            }
+            else
+                DisplayHelp();
+            */
+        }
+
+        /// <summary>
+        /// Prints credits and argument descriptions to the console.
+        /// </summary>
+        private static void DisplayHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("SuperBMD: A tool to import and export various 3D model formats into the Binary Model (BMD) format.");
+            Console.WriteLine("Written by Sage_of_Mirrors/Gamma (@SageOfMirrors) and Yoshi2/RenolY2.");
+            Console.WriteLine("Made possible with help from arookas, LordNed, xDaniel, and many others.");
+            Console.WriteLine("Visit https://github.com/Sage-of-Mirrors/SuperBMD/wiki for more information.");
+            Console.WriteLine();
+            Console.WriteLine("Usage: SuperBMD.exe -i/--input filePath [-o/--output filePath] [-m/materialPresets filePath]\n" +
+                              "       [-x/--texHeaders filePath] [-t/--tristrip mode] [-r/--rotate] [-f/--fixNormals]");
+            Console.WriteLine();
+            Console.WriteLine("Parameters:");
+            Console.WriteLine("\t-i/--input              filePath\tPath to the input file, either a BMD/BDL file or a DAE model.");
+            Console.WriteLine("\t-o/--output             filePath\tPath to the output file.");
+            Console.WriteLine("\t-m/--materialPresets    filePath\tPath to the material presets JSON for DAE to BMD conversion.");
+            Console.WriteLine("\t-x/--textureHeaders     filePath\tPath to the texture headers JSON for DAE to BMD conversion.");
+            Console.WriteLine("\t-t/--tristrip           mode\t\tMode for tristrip generation.");
+            Console.WriteLine("\t\tstatic: Only generate tristrips for static (unrigged) meshes.");
+            Console.WriteLine("\t\tall:    Generate tristrips for all meshes.");
+            Console.WriteLine("\t\tnone:   Do not generate tristrips.");
+            Console.WriteLine();
+            Console.WriteLine("\t-r/--rotate\t\t\t\tRotate the model from Z-up to Y-up orientation.");
+            Console.WriteLine("\t-f/fixNormals\t\t\t\tModify the normals in the mesh to facilitate proper in-game shading.");
+        }
+    }
+}
