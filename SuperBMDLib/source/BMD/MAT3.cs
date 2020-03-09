d271238cd6fdc0ddd8949da7e09d19e8f9617e7b
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-09 09:51:54 -0400

    Fix automatic material generation causing materials to get shuffled up

--- SuperBMDLib/source/BMD/MAT3.cs
+++ SuperBMDLib/source/BMD/MAT3.cs
@@ -323,7 +323,7 @@ namespace SuperBMDLib.BMD
             if (ambColorIndex != -1)
                 mat.AmbientColors[1] = m_AmbientColorBlock[ambColorIndex];
 
-            for (int i = 0; i  < 8; i++)
+            for (int i = 0; i < 8; i++)
             {
                 int lightIndex = reader.ReadInt16();
                 if ((lightIndex == -1) || (lightIndex > m_LightingColorBlock.Count) || (m_LightingColorBlock.Count == 0))
@@ -394,7 +394,7 @@ namespace SuperBMDLib.BMD
 
             for (int i = 0; i < 16; i++)
             {
-                mat.ColorSels[i] =  (KonstColorSel)reader.ReadByte();
+                mat.ColorSels[i] = (KonstColorSel)reader.ReadByte();
             }
 
             for (int i = 0; i < 16; i++)
@@ -543,12 +543,15 @@ namespace SuperBMDLib.BMD
 
         private void LoadFromScene(Assimp.Scene scene, TEX1 textures, SHP1 shapes)
         {
-            for (int i = 0; i < scene.MeshCount; i++)
+            for (int mat_index = 0; mat_index < scene.MaterialCount; mat_index++)
             {
-                Assimp.Material meshMat = scene.Materials[scene.Meshes[i].MaterialIndex];
+                var mesh = scene.Meshes.Find(m => m.MaterialIndex == mat_index);
+                int meshIndex = scene.Meshes.IndexOf(mesh);
+
+                Assimp.Material meshMat = scene.Materials[mesh.MaterialIndex];
                 Materials.Material bmdMaterial = new Material();
 
-                bool hasVtxColor0 = shapes.Shapes[i].AttributeData.CheckAttribute(GXVertexAttribute.Color0);
+                bool hasVtxColor0 = shapes.Shapes[meshIndex].AttributeData.CheckAttribute(GXVertexAttribute.Color0);
                 int texIndex = -1;
                 string texName = null;
                 if (meshMat.HasTextureDiffuse)
@@ -560,7 +563,7 @@ namespace SuperBMDLib.BMD
                 bmdMaterial.SetUpTev(meshMat.HasTextureDiffuse, hasVtxColor0, texIndex, texName);
 
                 m_Materials.Add(bmdMaterial);
-                m_RemapIndices.Add(i);
+                m_RemapIndices.Add(mat_index);
                 m_MaterialNames.Add(meshMat.Name);
             }
         }
@@ -571,7 +574,7 @@ namespace SuperBMDLib.BMD
 
             m_RemapIndices = new List<int>();
             m_MaterialNames = new List<string>();
-            
+
             m_IndirectTexBlock = new List<IndirectTexturing>();
             m_CullModeBlock = new List<CullMode>();
             m_MaterialColorBlock = new List<Color>();
